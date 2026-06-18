9c1960e1e3c41af28b8b934480be7072b334df74
DEV KUMAR SHARMA <sharmadev123123@gmail.com>
2026-06-18 22:35:18 +0530

    [dev]add: exceptionHandling

---
+++ core-java-practice/gcr-codebase/Exception_Handling/TryWithResourcesDemo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class TryWithResourcesDemo {
+    public static void main(String[] args) {
+
+        try (BufferedReader br =
+                     new BufferedReader(new FileReader("info.txt"))) {
+
+            String firstLine = br.readLine();
+
+            if (firstLine != null) {
+                System.out.println(firstLine);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
